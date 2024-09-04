06a016c428eee7fed42d97a7b8840fd709533a5c
kaustubh-ronge <166821543+kaustubh-ronge@users.noreply.github.com>
2024-09-04 11:04:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.getElementById('chat-form').addEventListener('submit', function (event)
 
 let userMessage = null;
 
-const API_KEY = `AIzaSyBeO1zvSnc0u04Tcb8arO3xa-671czDHJU`;
+const API_KEY = `YOUR_API_KEY`;
 
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
